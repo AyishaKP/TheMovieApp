dc8b412d560a5a1c44bd3cc712156a345f44eef3
Ayisha <ayishakp12@gmail.com>
2018-05-15 20:41:19 +0400

    Task1: Enhancements for requestable protocol
    Task1: Enhancements for requestable protocol

--- TheMovieApp/TheMovieApp/Managers/ServiceManager/Requestable+AlamofireObjectMapper.swift
+++ TheMovieApp/TheMovieApp/Managers/ServiceManager/Requestable+AlamofireObjectMapper.swift
@@ -12,18 +12,19 @@ import AlamofireObjectMapper
 import ObjectMapper
 
 extension Requestable {
-
     var `protocol`: String {
         return "http"
     }
     var baseUrl: String {
         return "api.themoviedb.org"
     }
-    var pathType: String {
+    var deployment: String {
         return "3"
     }
     var url: URL {
-        return URL(string: "\(`protocol`)://\(baseUrl)/\(pathType)") ?? URL(fileURLWithPath: "")
+        guard let url = URL(string: "\(`protocol`)://\(baseUrl)/\(deployment)")
+            else { fatalError("The url to call the Server is not configured") }
+        return url
     }
     var apiKey: String {
         return "2696829a81b1b5827d515ff121700838"
@@ -31,15 +32,20 @@ extension Requestable {
     var method: Alamofire.HTTPMethod {
         return .get
     }
-//    func call<T>(with responseObject: @escaping (DataResponse<T>) -> Void) -> DataRequest where T : BaseMappable {
-//       return
-//    }
-//    
-//    func call<T>(with responseArray: @escaping (DataResponse<[T]>) -> Void) -> DataRequest where T : BaseMappable {
-//        return
-//    }
-//    func asURLRequest() throws -> URLRequest {
-//        let urlRequest = try URLRequest(url: url.appendingPathComponent(path), method: method)
-//        return try Alamofire.URLEncoding.default.encode(urlRequest, with: parameters)
-//    }
+    func call<T>(with responseObjectCompletionHandler: @escaping (DataResponse<T>) -> Void)
+        -> DataRequest where T: BaseMappable {
+       return Alamofire.request(self).validate().responseObject(completionHandler: responseObjectCompletionHandler)
+    }
+    func call<T>(with responseArrayCompletionHandler: @escaping (DataResponse<[T]>) -> Void)
+        -> DataRequest where T: BaseMappable {
+        return Alamofire.request(self).validate().responseArray(completionHandler: responseArrayCompletionHandler)
+    }
+    func asURLRequest() throws -> URLRequest {
+        var urlRequest = URLRequest(url: url.appendingPathComponent(path))
+        urlRequest.httpMethod = method.rawValue
+        var finalParameters = parameters
+        finalParameters?.updateValue(apiKey, forKey: "api_key")
+        return try encoding.encode(urlRequest, with: finalParameters)
+
+    }
 }
--- TheMovieApp/TheMovieApp/Managers/ServiceManager/Requestable.swift
+++ TheMovieApp/TheMovieApp/Managers/ServiceManager/Requestable.swift
@@ -1,5 +1,5 @@
 //
-//  RequestableProtocol.swift
+//  Requestable.swift
 //  TheMovieApp
 //
 //  Created by Ayisha on 14/05/18.
@@ -14,12 +14,13 @@ protocol Requestable: URLRequestConvertible {
     var method: Alamofire.HTTPMethod { get }
     var `protocol`: String { get }
     var baseUrl: String { get }
-    var pathType: String { get }
+    var deployment: String { get }
     var path: String { get }
     var apiKey: String { get }
     var url: URL { get }
-    var parameters: Alamofire.Parameters { get }
+    var parameters: Parameters? { get }
     var encoding: ParameterEncoding { get }
+
     @discardableResult func call<T: BaseMappable>
         (with responseObject:@escaping (DataResponse<T>) -> Void) -> DataRequest
     @discardableResult func call<T: BaseMappable>
