e851ee0b9f7b5478ce72c0830f366fe3c978bc8e
Ayisha <ayishakp12@gmail.com>
2018-05-15 08:18:39 +0400

    Task1: Handling api calls using PromiseKit.

--- TheMovieApp/TheMovieApp/Managers/ServiceManager/Requestable+PromiseAPI.swift
+++ TheMovieApp/TheMovieApp/Managers/ServiceManager/Requestable+PromiseAPI.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import Alamofire
+import AlamofireObjectMapper
+import PromiseKit
+import ObjectMapper
+
+extension Requestable {
+
+    func request<T: BaseMappable>() -> Promise<T> {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        return Promise { (fulfil, reject) -> Void in
+            call { (response: DataResponse<T>) in
+
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+
+                guard response.error == nil else {
+                    reject(response.error ?? ServiceError.unknownError)
+                    return
+                }
+
+                guard let value = response.value else {
+                    reject(ServiceError.unknownError)
+                    return
+                }
+
+                fulfil(value)
+            }
+        }
+    }
+}
