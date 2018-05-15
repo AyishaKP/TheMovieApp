986e911a1b1b54e7975835dc8ebded580a4e9b8e
Ayisha <ayishakp12@gmail.com>
2018-05-15 08:14:34 +0400

    Added Extensions for custom colors, Added methods for requesting protocol and handling Api calls using promiseKit. Added Server Error handling.
    Task1: Added Extensions for custom colors

--- TheMovieApp/TheMovieApp/Extensions/UIColor+Extensions.swift
+++ TheMovieApp/TheMovieApp/Extensions/UIColor+Extensions.swift
@@ -10,4 +10,27 @@ import UIKit
 
 extension UIColor {
 
+    static let darkRed = UIColor(hex: 0xf80000)
+    static let transparentBlack = UIColor(hex: 0x000000, alpha: 0.5)
+    static let rgbRed = UIColor(red: 255, green: 0, blue: 0)
+
+    // Create a UIColor from RGB
+    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: alpha
+        )
+    }
+
+    // Create a UIColor from a hex value (E.g 0x000000)
+    convenience init(hex: Int, alpha: CGFloat = 1.0) {
+        self.init(
+            red: (hex >> 16) & 0xFF,
+            green: (hex >> 8) & 0xFF,
+            blue: hex & 0xFF,
+            alpha: alpha
+        )
+    }
 }
