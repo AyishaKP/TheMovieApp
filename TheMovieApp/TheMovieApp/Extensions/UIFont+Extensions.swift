151c6580bc493d6bf2650a1ae2404e64f05c9781
Ayisha <ayishakp12@gmail.com>
2018-05-16 03:40:56 +0400

    Added Lato Font

--- TheMovieApp/TheMovieApp/Extensions/UIFont+Extensions.swift
+++ TheMovieApp/TheMovieApp/Extensions/UIFont+Extensions.swift
@@ -10,4 +10,25 @@ import UIKit
 
 extension UIFont {
 
+    enum LatoFont: CustomStringConvertible {
+        case light
+        case regular
+        case medium
+        case bold
+        case black
+        var description: String {
+            var fontName: String = ""
+            switch self {
+            case .light: fontName = "Light"
+            case .regular: fontName = "Regular"
+            case .medium: fontName = "Medium"
+            case .bold: fontName = "Bold"
+            case .black: fontName = "Black"
+            }
+            return "Lato-" + fontName
+        }
+    }
+        class func latoFont(_ fontType: LatoFont, size: CGFloat) -> UIFont? {
+            return UIFont(name: fontType.description, size: size)
+        }
 }
